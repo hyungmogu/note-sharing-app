895030a28af6c6807c0c350e1e8cef95e2c77445
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-21 15:18:17 -0700

    feat: add title 'Login'

--- web/src/App.js
+++ web/src/App.js
@@ -7,12 +7,13 @@ function App() {
       <section
         className="section--page-loginSignup"
       >
-        <form className="form--type-loginSignup">
-          <input type="email" placeholder="Email"/>
-          <input type="password" placeholder="Password"/>
-          <button className="button button--type-primary" type="submit">Login</button>
-          <button className="button button--type-secondary" type="submit">Sign up</button>
-        </form>
+          <form className="form--type-loginSignup">
+            <h3>Note Sharing Application</h3>
+            <input type="email" placeholder="Email"/>
+            <input type="password" placeholder="Password"/>
+            <button className="button button--type-primary" type="submit">Login</button>
+            <button className="button button--type-secondary" type="submit">Sign up</button>
+          </form>
       </section>
     </div>
   );
