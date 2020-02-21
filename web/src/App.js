a17f61edb92012ee663889af5bde2b91ecb775c2
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-20 22:25:18 -0700

    feat: Add elements 'login'

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <form>
+          <input type="email" placeholder="Email"/>
+          <input type="password" placeholder="Password"/>
+          <button type="submit">Login</button>
+          <button type="submit">Sign up</button>
+        </form>
+      </section>
     </div>
   );
 }
