b1e5b01cc7c9e9babc4173768bd8450319110543
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-24 11:55:40 -0700

    feat: Add back button 'signUpScreen'

--- web/src/screens/SignUpScreen.js
+++ web/src/screens/SignUpScreen.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function SignUpScreen() {
-    return (
-        <section
-        className="section--page-loginSignup"
-        >
-            <form className="form--type-loginSignup">
-            <h3>Note Sharing Application</h3>
-            <input type="text" placeholder="Name"/>
-            <input type="email" placeholder="Email"/>
-            <input type="password" placeholder="Password"/>
-            <input type="password" placeholder="Password Confirm"/>
-            <button className="button button--type-primary" type="submit">Login</button>
-            <button className="button button--type-secondary" type="submit">Sign up</button>
-            </form>
-        </section>
-    );
+import {
+    NavLink
+  } from 'react-router-dom';
+
+
+class SignUpScreen extends Component {
+    render() {
+        return (
+            <section
+            className="section--page-loginSignup"
+            >
+                <form className="form--type-loginSignup">
+                <h3>Note Sharing Application</h3>
+                <input type="text" placeholder="Name"/>
+                <input type="email" placeholder="Email"/>
+                <input type="password" placeholder="Password"/>
+                <input type="password" placeholder="Password Confirm"/>
+                <button className="button button--type-primary" type="submit">Login</button>
+                <NavLink exact to="/" className="button button--type-secondary">Back</NavLink>
+                </form>
+            </section>
+        );
+    }
 }
 
 export default SignUpScreen;
