27e56115fbaddd1b2346d76f234d9956e6bd68b9
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-24 15:17:27 -0700

    refactor: replace color with variable 'app.scss'

--- web/src/components/Header.js
+++ web/src/components/Header.js
@@ -6,17 +6,19 @@ import {
 
 
 const Header = () => (
-  <header class="header--navigation-primary">
-    <section>
-        <button class="button--type-add">+</button>
-    </section>
-    <ul className="main-nav">
-      <li><NavLink to="#">Physics</NavLink></li>
-      <li><NavLink to="#">Social Studies</NavLink></li>
-      <li><NavLink to="#">Biology</NavLink></li>
-      <li><NavLink to="#">English</NavLink></li>
-    </ul>
-  </header>
+    <header className="header--navigation-primary">
+        <nav className="nav--type-desktop">
+            <section>
+                <button class="button--type-add">+</button>
+            </section>
+            <ul className="main-nav">
+                <li><NavLink to="#">Physics</NavLink></li>
+                <li><NavLink to="#">Social Studies</NavLink></li>
+                <li><NavLink to="#">Biology</NavLink></li>
+                <li><NavLink to="#">English</NavLink></li>
+            </ul>
+        </nav>
+    </header>
 );
 
 export default Header;
