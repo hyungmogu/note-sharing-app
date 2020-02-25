61cda68a3033668d4223d46662c02e1c62346c06
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-25 14:05:29 -0700

    refactor: move Header to App

--- web/src/App.js
+++ web/src/App.js
@@ -6,7 +6,9 @@ import {
   Switch
 } from 'react-router-dom';
 
+import Header from './components/Header';
 import HeaderMobile from './components/HeaderMobile';
+
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import SignUpScreen from './screens/SignUpScreen';
@@ -16,6 +18,7 @@ function App() {
     return (
         <BrowserRouter basename={"note-sharing-app"}>
             <HeaderMobile/>
+            <Header/>
             <div className="App">
                 <Switch>
                     <Route exact path="/" component={LoginScreen}/>
