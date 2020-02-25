aefdd667997711a9d59915ac124454ce113b1dff
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-25 13:40:06 -0700

    style: remove extraneous spacings in screens due to mobile header

--- web/src/components/HeaderMobile.js
+++ web/src/components/HeaderMobile.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 
