fd5ce650c4ac3ae750467d09b3234bd40a913c17
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-25 09:59:45 -0700

    fix: indent elements 'SignUpScreen'

--- web/src/screens/SignUpScreen.js
+++ web/src/screens/SignUpScreen.js
@@ -12,13 +12,13 @@ class SignUpScreen extends Component {
             className="section--screen section--screen-loginSignup"
             >
                 <form className="form--type-loginSignup">
-                <h3>Note Sharing Application</h3>
-                <input type="text" placeholder="Name"/>
-                <input type="email" placeholder="Email"/>
-                <input type="password" placeholder="Password"/>
-                <input type="password" placeholder="Password Confirm"/>
-                <button className="button button--type-primary" type="submit">Login</button>
-                <NavLink exact to="/" className="button button--type-secondary">Back</NavLink>
+                    <h3>Note Sharing Application</h3>
+                    <input type="text" placeholder="Name"/>
+                    <input type="email" placeholder="Email"/>
+                    <input type="password" placeholder="Password"/>
+                    <input type="password" placeholder="Password Confirm"/>
+                    <button className="button button--type-primary" type="submit">Login</button>
+                    <NavLink exact to="/" className="button button--type-secondary">Back</NavLink>
                 </form>
             </section>
         );
