5e6d866defb3e38e7efc773b3688031602a157b2
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-26 15:54:35 -0700

    feat: Add file 'SecondaryHeader'

---
+++ web/src/components/SecondaryHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SecondaryHeader extends Component {
+
+    render() {
+        return (
+            <header className="header--navigation-secondary">
+
+            </header>
+        );
+    }
+};
+
+export default SecondaryHeader;
