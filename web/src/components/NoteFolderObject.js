eb43c0de3ba6c2ae01f52e67eeaddce6a0c1d979
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-27 10:07:15 -0700

    feat: Add file 'NoteFolderObject'

---
+++ web/src/components/NoteFolderObject.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class NoteFolderObject extends Component {
+
+    render() {
+        return (
+            <div className="noteFolder">
+
+            </div>
+        );
+    }
+};
+
+export default NoteFolderObject;
