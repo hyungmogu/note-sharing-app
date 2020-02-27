b5121aa1e0947e7680ae9aa28e45b47b46f8503f
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-26 18:29:39 -0700

    feat: add Add User button 'SecondaryHeader'

--- web/src/components/SecondaryHeader.js
+++ web/src/components/SecondaryHeader.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons'
+import { faPlus, faUserCircle } from '@fortawesome/free-solid-svg-icons'
 
 class SecondaryHeader extends Component {
 
@@ -12,6 +12,12 @@ class SecondaryHeader extends Component {
                     Physics
                 </h3>
                 <ul>
+                    <li>
+                        <button className="button button--type-addUser">
+                            <FontAwesomeIcon icon={faPlus}/>
+                            Add User
+                        </button>
+                    </li>
                     <li>
                         <button className="button button--type-user">
                             <FontAwesomeIcon icon={faUserCircle}/>
