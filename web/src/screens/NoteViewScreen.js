f2a3e92dfaba9d258c7ceaaa755ee6b5a4e2da57
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-28 11:47:56 -0700

    feat: Add code 'NoteViewScreen'

--- web/src/screens/NoteViewScreen.js
+++ web/src/screens/NoteViewScreen.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 import { withRouter } from 'react-router-dom';
+import SecondaryHeader from '../components/SecondaryHeader';
 
 class NoteViewScreen extends Component {
 
     render() {
-        return;
+        return (
+            <section
+            className="section--screen section--screen-view"
+            >
+                <SecondaryHeader/>
+                <main className="main--type-content">
+                    <img src={this.props.imageURL}/>
+                    <section>
+                        <button>&lt;Back</button>
+                        <span>Page 1</span>
+                        <button>Next&gt;</button>
+                    </section>
+                </main>
+            </section>
+        );
     }
 }
 
