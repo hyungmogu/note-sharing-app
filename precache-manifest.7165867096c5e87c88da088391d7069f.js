87becef690116e1c58b485e1ebd28b3c8db4f28f
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-28 13:02:48 -0700

    Updates

---
+++ precache-manifest.7165867096c5e87c88da088391d7069f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68d19c6db1d8d879a1590b3c8ab43c4d",
+    "url": "/note-sharing-app/index.html"
+  },
+  {
+    "revision": "1280d230445cfa3ecb45",
+    "url": "/note-sharing-app/static/css/main.10c91c86.chunk.css"
+  },
+  {
+    "revision": "9c1962567005c59d9358",
+    "url": "/note-sharing-app/static/js/2.09893d45.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/note-sharing-app/static/js/2.09893d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1280d230445cfa3ecb45",
+    "url": "/note-sharing-app/static/js/main.6cd90e17.chunk.js"
+  },
+  {
+    "revision": "31c4128c9aeecf944f86",
+    "url": "/note-sharing-app/static/js/runtime-main.de863928.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/note-sharing-app/precache-manifest.929215c916127a9159016279cea921bc.js"
+  "/note-sharing-app/precache-manifest.7165867096c5e87c88da088391d7069f.js"
 );
 
 self.addEventListener('message', (event) => {
