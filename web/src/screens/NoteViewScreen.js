4b2f41e06ec26c611b5d0a1a9a08b08184e9b0ac
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-28 09:47:55 -0700

    feat: Add file 'NoteViewScreen'

---
+++ web/src/screens/NoteViewScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import { withRouter } from 'react-router-dom';
+
+class NoteViewScreen extends Component {
+
+    render() {
+        return;
+    }
+}
+
+export default withRouter(NoteViewScreen);
