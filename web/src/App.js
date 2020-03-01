c4a1b60a52bf67bfd649dd7403b312d22e36e516
Hyungmo Gu <guhyungm7@gmail.com>
2020-02-29 18:51:43 -0700

    feat: import and add app provider 'App'

--- web/src/App.js
+++ web/src/App.js
@@ -6,6 +6,8 @@ import {
   Switch
 } from 'react-router-dom';
 
+import { AppProvider } from './components/Context';
+
 import NoteViewHeader from './components/NoteViewHeader';
 import PrimaryHeader from './components/PrimaryHeader';
 import PrimaryHeaderMobile from './components/PrimaryHeaderMobile';
@@ -21,21 +23,23 @@ import './App.scss';
 class App extends Component {
     render() {
         return (
-            <BrowserRouter basename={"note-sharing-app"}>
-                <div className="App">
-                    <NoteViewHeader/>
-                    <PrimaryHeaderMobile/>
-                    <PrimaryHeader/>
-                    <Switch>
-                        <Route path="/login" component={LoginScreen}/>
-                        <Route path="/signup" component={SignUpScreen}/>
-                        <PrivateRoute path="/home" component={HomeScreen}/>
-                        <PrivateRoute path="/notes/:note/:folder/:page" component={NoteViewScreen}/>
-                        <PrivateRoute path="/notes/:note/:folder" component={DirectoryScreen}/>
-                        <PrivateRoute path="/notes/:note" component={DirectoryScreen}/>
-                    </Switch>
-                </div>
-            </BrowserRouter>
+            <AppProvider>
+                <BrowserRouter basename={"note-sharing-app"}>
+                    <div className="App">
+                        <NoteViewHeader/>
+                        <PrimaryHeaderMobile/>
+                        <PrimaryHeader/>
+                        <Switch>
+                            <Route path="/login" component={LoginScreen}/>
+                            <Route path="/signup" component={SignUpScreen}/>
+                            <PrivateRoute path="/home" component={HomeScreen}/>
+                            <PrivateRoute path="/notes/:note/:folder/:page" component={NoteViewScreen}/>
+                            <PrivateRoute path="/notes/:note/:folder" component={DirectoryScreen}/>
+                            <PrivateRoute path="/notes/:note" component={DirectoryScreen}/>
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </AppProvider>
         );
     }
 }
