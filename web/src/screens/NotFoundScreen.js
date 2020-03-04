7304789a0ea688d7a7a69aed5ad47061e19f7446
Hyungmo Gu <guhyungm7@gmail.com>
2020-03-04 14:15:01 -0700

    feat: Add secondary header 'NotFoundScreen'

--- web/src/screens/NotFoundScreen.js
+++ web/src/screens/NotFoundScreen.js
@@ -4,11 +4,15 @@ import { withRouter } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBolt } from '@fortawesome/free-solid-svg-icons';
 
+import SecondaryHeader from '../components/SecondaryHeader';
+
+
 class NotFoundScreen extends Component {
 
     render() {
         return(
             <section className="screen screen--notFound">
+                <SecondaryHeader/>
                 <FontAwesomeIcon className="screenIcon" icon={faBolt}/>
                 <h2 className="title">Page is Not Found</h2>
             </section>
