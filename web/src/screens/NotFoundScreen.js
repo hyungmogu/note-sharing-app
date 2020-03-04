a540528be84cf135c2d0d67aebf5f5e4a754d511
Hyungmo Gu <guhyungm7@gmail.com>
2020-03-04 13:47:23 -0700

    feat: Add content 'NotFoundScreen'

--- web/src/screens/NotFoundScreen.js
+++ web/src/screens/NotFoundScreen.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
 import { withRouter } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBolt } from '@fortawesome/free-solid-svg-icons';
 
 class NotFoundScreen extends Component {
 
     render() {
         return(
             <section className="screen--notFound">
-
+                <FontAwesomeIcon icon={faBolt}/>
+                <h3>Page is Not Found</h3>
             </section>
         );
     }
