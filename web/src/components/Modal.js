3b7faed600eac68746197ca046ce4d65823b45cd
Hyungmo Gu <guhyungm7@gmail.com>
2020-03-05 14:01:06 -0700

    feat: add base code 'Modal'

---
+++ web/src/components/Modal.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import { NavLink, withRouter } from 'react-router-dom';
+
+class Modal extends Component {
+
+    render() {
+        return;
+    }
+};
+
+export default withRouter(Modal);
