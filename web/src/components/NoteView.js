5d6661806be6b7536085307fe778bfc3d39f7f0b
Hyungmo Gu <guhyungm7@gmail.com>
2020-03-04 19:06:55 -0700

    feat: Add class names 'navigation--btnWrapper' and 'navigation--textWrapper' 'NoteView'

--- web/src/components/NoteView.js
+++ web/src/components/NoteView.js
@@ -29,13 +29,19 @@ class NoteView extends Component {
             <section className="noteView">
                 <img src="https://via.placeholder.com/750x1000"/>
                 <section className="navigation">
-                    { this.state.currentIndex === 0 ? ''
-                        : <button onClick={this.handlePrev}>&lt; Back</button>
-                    }
-                    <span>Page 1</span>
-                    { this.state.currentIndex == this.props.appContext.pages.items.length - 1 ? ''
-                        : <button onClick={this.handleNext}>Next &gt;</button>
-                    }
+                    <section className="navigation--btnWrapper">
+                        { this.state.currentIndex === 0 ? ''
+                            : <button onClick={this.handlePrev}>&lt; Back</button>
+                        }
+                    </section>
+                    <section className="navigation--textWrapper">
+                        <span>Page 1</span>
+                    </section>
+                    <section className="navigation--btnWrapper">
+                        { this.state.currentIndex == this.props.appContext.pages.items.length - 1 ? ''
+                            : <button onClick={this.handleNext}>Next &gt;</button>
+                        }
+                    </section>
                 </section>
             </section>
         );
