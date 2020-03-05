32e7341c25219db9dcf684305d788fb0de646b92
Hyungmo Gu <guhyungm7@gmail.com>
2020-03-04 19:56:23 -0700

    feat: update image on clicking next and prev btn 'NoteView'

--- web/src/components/NoteView.js
+++ web/src/components/NoteView.js
@@ -32,15 +32,17 @@ class NoteView extends Component {
 
     render() {
 
+        let image = '';
         let name = '';
         try {
             name = this.props.appContext.pages.items[this.state.currentIndex].name;
+            image = this.props.appContext.pages.items[this.state.currentIndex].imageURL;
         } catch(Error) {}
 
 
         return (
             <section className="noteView">
-                <img src="https://via.placeholder.com/750x1000"/>
+                <img src={image}/>
                 <section className="navigation">
                     <section className="navigation--btnWrapper">
                         { this.state.currentIndex === 0 ? ''
