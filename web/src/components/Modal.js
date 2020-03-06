0022c5eec8c3ad3b85ab5b1e2996ed7c70ef0679
Hyungmo Gu <guhyungm7@gmail.com>
2020-03-05 19:01:34 -0700

    feat: hide modal on clicking background 'Modal'

--- web/src/components/Modal.js
+++ web/src/components/Modal.js
@@ -3,13 +3,21 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 
 class Modal extends Component {
+    modalRef = React.createRef();
 
     closeModal = (e) => {
+        if (!this.modalRef.current.classList.contains('open')) {
+            this.modalRef.current.classList.add('closed');
+        }
     }
 
     render() {
         return (
-            <section className="modal" onClick={e => this.closeModal(e)}>
+            <section
+                ref={this.modalRef}
+                className="modal"
+                onClick={e => this.closeModal(e)}
+            >
                 <section className="modal--wrapper" onClick={e => e.stopPropagation()}>
                     {this.props.children}
                 </section>
